829e3c7aee9dbeef1485207c89860bd763e84e96
Jonathan Gardner <Jonathan.Gardner@Redspace.com>
2018-10-02 15:24:19 -0300

    Started on CreatJS

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './styles.css';
 import 'springroll';
+import { create } from 'domain';
+
+let stage = new createjs.Stage('stage');
+var shape = new createjs.Shape();
+shape.graphics.beginFill('red').drawRect(0, 0, 120, 120);
+stage.addChild(shape);
+stage.update();
