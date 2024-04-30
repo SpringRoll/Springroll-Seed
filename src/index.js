e14a19af1a36c4d4b100d40414479e8a88824811
Jeremy McCurdy <jeremy.mccurdy@redspace.com>
2024-04-30 13:10:12 -0300

    Updated application initialization to be async per the Pixi 8 standard. `game.js` now dispatches an event before `.run` is called by `index.js`

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import './styles.css';
 import { Game } from './game';
 import { GAMEPLAY } from './constants';
 
+// eslint-disable-next-line no-unused-vars
 const template = new Game(GAMEPLAY.WIDTH, GAMEPLAY.HEIGHT);
-template.run();
+template.emitter.once('ready', () =>
+{
+    template.run();
+});
