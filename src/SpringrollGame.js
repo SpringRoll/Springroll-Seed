f5de1315ea07095ac0082171ecdc5c7b33cb9f4b
Jeremy McCurdy <jeremy.mccurdy@redspace.com>
2024-05-01 13:56:06 -0300

    Updated Phaser to version 3.80.1, adjusted startup flow and SpringRoll plugins to prevent errors.

--- src/SpringrollGame.js
+++ src/SpringrollGame.js
@@ -6,64 +6,57 @@ import Phaser from 'phaser';
 
 class SpringrollGame {
     constructor() {
+        // Create a Phaser.Game.
+        this.game = new Phaser.Game({
+            type: Phaser.AUTO,
+            width: GAMEPLAY.WIDTH,
+            height: GAMEPLAY.HEIGHT,
+            backgroundColor: '#000000',
+            parent: 'gameTarget',
+            plugins: {
+                // FactoryPlugin is not necessary for Springroll, however it demonstrates
+                // how to setup and install a Phaser.Plugin.
+                global: [{ key: "FactoryPlugin", plugin: FactoryPlugin, start: true }]
+            }
+        });
+
+        // Create a Springroll.SafeScaleManager.
+        this.safeScale = new SafeScaleManager({
+            width: GAMEPLAY.WIDTH,
+            height: GAMEPLAY.HEIGHT,
+            safeWidth: GAMEPLAY.SAFE_WIDTH,
+            safeHeight: GAMEPLAY.SAFE_HEIGHT,
+            callback: this.onWindowResize.bind(this)
+        });
+
+        // Add game scenes.
+        this.game.scene.add(SCENE.GAME, GameScene);
+        this.game.scene.add(SCENE.TITLE, TitleScene, true);
+
+
         // Instance of a Springroll.Application.
         // Flag any additional features. See https://github.com/SpringRoll/SpringRoll/tree/master/src
         this.application = new Application({
             features: {
-                sfx: true
+                pause: true,
+                soundVolume:true,
+                musicVolume: true,
+                voVolume: true,
             }
         });
 
-        // Instance of a Phaser.Game.
-        // This will be initialized when the application is ready.
-        this.game = undefined;
-
-        // Listen for when the application is ready.
-        this.application.state.ready.subscribe(this.onSpringrollApplicationReady.bind(this));
-    }
-
-    onSpringrollApplicationReady(isReady) {
-        if (isReady) {
-            // Listen for container events from the application.
-            this.application.state.pause.subscribe(this.onApplicationPause.bind(this));
-            this.application.state.soundVolume.subscribe(this.onMasterVolumeChange.bind(this));
-            this.application.state.musicVolume.subscribe(result => {
-                console.log('musicVolume: ', result);
-            });
-            this.application.state.voVolume.subscribe(result => {
-                console.log('voVolume: ', result);
-            });
-            this.application.state.captionsMuted.subscribe(result => {
-                console.log('captionsMuted: ', result);
-            });
-
-            // Create a Phaser.Game.
-            this.game = new Phaser.Game({
-                type: Phaser.AUTO,
-                width: GAMEPLAY.WIDTH,
-                height: GAMEPLAY.HEIGHT,
-                backgroundColor: '#000000',
-                parent: 'gameTarget',
-                plugins: {
-                    // FactoryPlugin is not necessary for Springroll, however it demonstrates
-                    // how to setup and install a Phaser.Plugin.
-                    global: [{ key: "FactoryPlugin", plugin: FactoryPlugin, start: true }]
-                }
-            });
-
-            // Create a Springroll.SafeScaleManager.
-            this.safeScale = new SafeScaleManager({
-                width: GAMEPLAY.WIDTH,
-                height: GAMEPLAY.HEIGHT,
-                safeWidth: GAMEPLAY.SAFE_WIDTH,
-                safeHeight: GAMEPLAY.SAFE_HEIGHT,
-                callback: this.onWindowResize.bind(this)
-            });
-
-            // Add game scenes.
-            this.game.scene.add(SCENE.GAME, GameScene);
-            this.game.scene.add(SCENE.TITLE, TitleScene, true);
-        }
+        // Listen for container events from the application.
+        this.application.state.pause.subscribe(this.onApplicationPause.bind(this));
+        this.application.state.soundVolume.subscribe(this.onMasterVolumeChange.bind(this));
+        this.application.state.musicVolume.subscribe(result => {
+            console.log('musicVolume: ', result);
+        });
+        this.application.state.voVolume.subscribe(result => {
+            console.log('voVolume: ', result);
+        });
+        this.application.state.captionsMuted.subscribe(result => {
+            console.log('captionsMuted: ', result);
+        });
     }
 
     onApplicationPause(value) {
